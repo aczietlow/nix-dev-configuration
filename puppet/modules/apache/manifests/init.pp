65bad5e055bab439bdb5fdcc01ec4fc9390eae75
Chris Zietlow <zietlow@palantir.net>
2014-05-08 00:44:37 -0400

    Used custom apache2.conf for apache set up.

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -2,4 +2,13 @@ class apache {
   package {'apache2':
     ensure: present,
   }
+
+  file {'/etc/apache2/apache2.conf':
+  	ensure => file,
+  	owner => root,
+  	group => root,
+  	mode => 644,
+  	soure => 'puppet:///modules/apache/apache2.conf'
+  	require => Package['apache2']
+  }
 }
