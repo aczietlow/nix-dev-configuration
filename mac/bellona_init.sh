eadb9a4c4d38f42c3e140eade09f07b5e98f1f52
aczietlow <aczietlow@gmail.com>
2015-08-06 10:06:29 -0400

    can install ssh-copy-id with brew.

--- mac/bellona_init.sh
+++ mac/bellona_init.sh
@@ -34,6 +34,7 @@ brew install node
 brew install heroku-toolbelt
 brew install qt --HEAD
 brew install wkhtmltopdf
+brew install ssh-copy-id
 gem install jekyll bundler
 brew tap homebrew/dupes
 brew tap homebrew/versions
@@ -65,8 +66,6 @@ ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
 
 # Use my bash profile.
 cd ~/ && { curl -fsSLO https://raw.githubusercontent.com/aczietlow/nix-dev-configuration/master/mac/conf/.bash_profile ; cd -; }
-
-curl -L https://raw.githubusercontent.com/beautifulcode/ssh-copy-id-for-OSX/master/install.sh | sh
 
 # Reminder of what's left to be done.
 echo "Now that that's done I need you to:
